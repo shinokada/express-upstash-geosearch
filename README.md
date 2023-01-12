7644af29cec2335837e9fa731f33e7136f5c55d7
Shinichi Okada <okada.shin@gmail.com>
2023-01-12 12:46:35 +0900

    feat: add speed check and update index

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Express-Redis-Upstash
+# Express-Upstash-Geosearch
 
 - Express
 - ES modules
@@ -11,5 +11,12 @@
 ## Installation
 
 ```bash
-git clone 
+git clone git@github.com:shinokada/express-upstash-geosearch.git
+cd express-upstash-geosearch
+# npm/pnpm/bun/yarn
+pnpm i
 ```
+
+### .env
+
+
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from 'express';
+import path from 'path';
+import { v4 } from 'uuid';
+import { fileURLToPath } from 'url';
 import Bodyparser from 'body-parser';
 import { fromNorthPole, aroundLockm, nearbyCities, client } from './src/redis.js';
 import { addUsCities } from './src/uscities.js';
@@ -10,7 +11,7 @@ client.flushall();
 addUsCities();
 addIntCities();
 
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(Bodyparser.json());
 
@@ -19,32 +20,40 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, '/public/')))
+app.use((req, res, next) => {
+  req.id = v4();
+  console.time(`request: ${req.id}`);
+  next();
+});
 
 // set view engine and view path
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 // app.set('view engine', 'pug')
 // app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  const title = 'Express Redis Upstash'
-  res.render('index', { title: title })
+  const title = 'Express Redis Upstash';
+  res.render('index', { title: title });
+  console.timeEnd(`request: ${req.id}`);
 })
 
 app.get('/location', (req, res) => {
-  const title = 'Location'
-  res.render('location', { title })
+  const title = 'Location';
+  res.render('location', { title });
+  console.timeEnd(`request: ${req.id}`);
 })
 
 app.post("/api/location", (req, res) => {
   const { latitude, longitude } = req.body;
-  res.send({ latitude, longitude })
+  res.send({ latitude, longitude });
 });
 
 app.get('/nearby-cities/:distance', (req, res) => {
-  const distance = req.params.distance
-  const title = 'Nearby cities'
-  res.render('nearbycities', { distance: distance, title: title })
+  const distance = req.params.distance;
+  const title = 'Nearby cities';
+  res.render('nearbycities', { distance: distance, title: title });
+  console.timeEnd(`request: ${req.id}`);
 })
 
 app.post('/nearby-cities', async (req, res) => {
@@ -52,24 +61,26 @@ app.post('/nearby-cities', async (req, res) => {
   const latitude = parseInt(req.body.latitude);
   const longitude = parseInt(req.body.longitude);
   const distance = parseInt(req.body.distance);
-  const cities = await nearbyCities(longitude, latitude, distance)
-  res.send(cities)
+  const cities = await nearbyCities(longitude, latitude, distance);
+  res.send(cities);
 });
 
 app.get('/from-northpole/:km', async (req, res) => {
-  const distance = parseInt(req.params.km)
+  const distance = parseInt(req.params.km);
   const data = await fromNorthPole(distance);
   // console.log(typeof data)
   const title = `Cities within ${distance} km of the North Pole`
   res.render('from-northpole', { data, title });
+  console.timeEnd(`request: ${req.id}`);
 });
 
 app.get('/around/:long/:lat/:km', async (req, res) => {
   const data = await aroundLockm(parseInt(req.params.long), parseInt(req.params.lat), parseInt(req.params.km));
   const title = 'Find Cities Near a Specific Location using Longitude and Latitude Coordinates'
   res.render('around', { data, title });
+  console.timeEnd(`request: ${req.id}`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Example app listening on port ${PORT}`);
 })
